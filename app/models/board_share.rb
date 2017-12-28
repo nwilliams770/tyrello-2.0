f1b81f3d775c53fc182f2d848cb3d4930159d1d8
Nicholas <nwilliams770@gmail.com>
2017-12-27 20:55:45 -0800

    complete board_share model

--- app/models/board_share.rb
+++ app/models/board_share.rb
@@ -1,2 +1,11 @@
 class BoardShare < ApplicationRecord
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :contributor_id,
+    class_name: :User
+
+  belongs_to :board,
+    primary_key: :id,
+    foreign_key: :board_id,
+    class_name: :Board
 end
