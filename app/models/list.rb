be4497cff70140805022cc3f5b719e2c78105d2b
Nicholas <nwilliams770@gmail.com>
2017-12-27 20:53:50 -0800

    complete list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,11 @@
 class List < ApplicationRecord
+    belongs_to :board,
+    primary_key: :id,
+    foreign_key: :board_id,
+    class_name: :Board
+
+  has_many :cards,
+    primary_key: :id,
+    foreign_key: :list_id,
+    class_name: :Card
 end
