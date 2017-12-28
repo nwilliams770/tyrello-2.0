ed8b0fe7598eb868b39845b3aa27ab1998e0822d
Nicholas <nwilliams770@gmail.com>
2017-12-27 20:55:14 -0800

    complete board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,11 @@
 class Board < ApplicationRecord
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: :User
+
+  has_many :lists,
+    primary_key: :id,
+    foreign_key: :board_id,
+    class_name: :List
 end
