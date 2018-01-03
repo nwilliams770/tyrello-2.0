00cd8fd78fa013394d12d539e3122e88e124a325
Nicholas <nwilliams770@gmail.com>
2018-01-03 10:37:08 -0800

    add session form container and component

---
+++ frontend/components/splash_page/session_form/session_form_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+
+import { login, clearErrors } from '../../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: Boolean(state.session.currentUser),
+    errors: state.errors.session
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: user => dispatch(login(user)),
+  clearErrors: () => dispatch(clearErrors())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SessionForm);
