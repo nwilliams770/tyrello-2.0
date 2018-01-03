b424927ef97b469e00d340271dd83c2ca826f325
Nicholas <nwilliams770@gmail.com>
2018-01-02 18:45:50 -0800

    add session_errors and errors reducers

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,21 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_SESSION_ERRORS,
+  CLEAR_ERRORS
+} from '../actions/session_actions';
+
+const _nullErrors = [];
+
+export default (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case CLEAR_ERRORS:
+      return _nullErrors;
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return _nullErrors;
+    default:
+      return state;
+  }
+};
