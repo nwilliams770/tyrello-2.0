47beb9da86b4a383445efab66cb1f93d5c6cdfdf
Nicholas <nwilliams770@gmail.com>
2018-01-03 10:39:40 -0800

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'es2016']
+          presets: ['react', 'es2015']
         }
       }
     ]
