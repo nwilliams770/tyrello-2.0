56ccc1d5eb423dfe0ee17490df623fd3c906b5ee
Nicholas <nwilliams770@gmail.com>
2018-01-05 12:59:55 -0800

    add favicon and video background

--- frontend/components/splash_page/session_form/session_form.jsx
+++ frontend/components/splash_page/session_form/session_form.jsx
@@ -53,7 +53,9 @@ class SessionForm extends React.Component {
   render() {
     return (
       <div>
-       <h1> testing testing </h1>
+        <video autoPlay loop className="fullscreen-bg fullscreen-bg-video">
+          <source src="http://res.cloudinary.com/nwilliams770/video/upload/v1515178864/Bee_Flower_Compressed_dkfzal.mp4" type="video/mp4" />
+        </video>
       </div>
     );
 
