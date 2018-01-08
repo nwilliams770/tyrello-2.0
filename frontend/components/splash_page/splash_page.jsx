0b0f0f6e988edbc376792929bab65bce69eb6f62
Nicholas <nwilliams770@gmail.com>
2018-01-08 10:48:22 -0800

    refactor splash components

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -7,6 +7,9 @@ class SplashPage extends React.Component {
   render() {
     return (
       <div>
+        <video autoPlay loop className="fullscreen-bg fullscreen-bg-video">
+          <source src="http://res.cloudinary.com/nwilliams770/video/upload/v1515178864/Bee_Flower_Compressed_dkfzal.mp4" type="video/mp4" />
+        </video>
         <SessionFormContainer />
       </div>
     );
