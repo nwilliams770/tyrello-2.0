81792d3569f197667d119e48632172a68edd1b3b
Nicholas <nwilliams770@gmail.com>
2018-01-17 15:59:03 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Board.destroy_all
+BoardShare.destroy_all
+List.destroy_all
+Card.destroy_all
+
+user1 = User.create(username: "Sansa-Stark", email: "SansaStark@me.com", password: "password")
+user2 = User.create(username: "Arya-Stark", email: "AryaStark@me.com", password: "password2")
+
+board1 = Board.create(name: "Revenge List", author_id: user2.id)
+board2 = Board.create(name: "White Walkers 101", author_id: user1.id)
+
+list1 = List.create(title: "Frost-Resistant Weapons", board_id: board2.id)
+list3 = List.create(title: "Undead Dragon Recipes", board_id: board2.id)
+# list4 = List.create(title: "where the lady whitewalkers at?", board_id: board2.id)
+# list5 = List.create(title: "the Night King", board_id: board2.id)
+# list6 = List.create(title: "wights and how to make them work FOR YOU", board_id: board2.id)
+card1= Card.create(title: "Dragon Glass", list_id: list1.id)
+card2 = Card.create(title: "Valyrian Steel", list_id: list1.id)
+card6 = Card.create(title: "Dragon Fire!", list_id: list1.id)
+
+card3 = Card.create(title: "Dead Dragon Bone", list_id: list3.id)
+card4 = Card.create(title: "Fermented Dragon Paté", list_id: list3.id)
+card5 = Card.create(title: "Dragon Eye Schmear", list_id: list3.id)
+
+list2 = List.create(title: "Those Who Have Been Dealt With", board_id: board1.id)
+list4 = List.create(title: "Those Needing A Smackdown", board_id: board1.id)
+
+card7 = Card.create(title: "Joffrey", list_id: list2.id)
+card8 = Card.create(title: "Meryn Trant", list_id: list2.id)
+card9 = Card.create(title: "Walder Frey", list_id: list2.id)
+card10 = Card.create(title: "Cersei", list_id: list4.id)
+card11 = Card.create(title: "Ilyn Payne", list_id: list4.id)
+card12 = Card.create(title: "The Red Woman", list_id: list4.id)
+
+BoardShare.create(board_id: board1.id, contributor_id: user1.id)
