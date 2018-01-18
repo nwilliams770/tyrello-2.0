1e7f0c2f70f42c556675f946a30254f5d47c1d7b
Nicholas <nwilliams770@gmail.com>
2018-01-17 19:29:06 -0800

    add boards index view

--- Gemfile
+++ Gemfile
@@ -30,6 +30,10 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'jquery-rails'
+gem 'font-awesome-sass', '~> 4.7.0'
+gem 'bcrypt', '~> 3.1.7'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -50,7 +54,6 @@ group :development do
   gem 'binding_of_caller'
   gem 'pry-rails'
   gem 'annotate'
-  gem 'jquery-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ app/controllers/api/boards_controller.rb
@@ -0,0 +1,85 @@
+class Api::BoardsController < ApplicationController
+  def create
+    @board = Board.new(board_params)
+    @board.author_id = current_user.id
+
+    if @board.save!
+      render :index
+    else
+      render :json, @board.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @boards = current_user.boards
+    @shared_boards = current_user.shared_boards
+
+    @board_ids = []
+    @boards.each do |board|
+      @board_ids << board.id
+    end
+
+    @shared_board_ids = []
+    @shared_boards.each do |board|
+      @shared_board_ids << board.id
+    end
+
+    @list_ids = []
+    @boards.each do |board|
+      board.lists.each do |list|
+        @list_ids << list.id
+      end
+    end
+    render :index
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    @boards = current_user.boards
+    @shared_boards = current_user.shared_boards
+
+    @board_ids = []
+    @boards.each do |board|
+      @board_ids << board.id
+    end
+
+    @shared_board_ids = []
+    @shared_boards.each do |board|
+      @shared_board_ids << board.id
+    end
+
+    @lists = @board.lists
+    @list_ids = []
+    @board.lists.each do |list|
+      @list_ids << list.id
+    end
+
+    @cards = []
+      @board.lists.each do |list|
+        list.cards.each do |card|
+          @cards << card
+        end
+      end
+    render :show
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+    if @board.save!
+      render :json
+    else
+      render :json, @board.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy!
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
+end
